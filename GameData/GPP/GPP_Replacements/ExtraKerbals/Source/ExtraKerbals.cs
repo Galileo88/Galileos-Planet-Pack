3712c8f3a9de0bf7edde9114e3129422135e47ae
Galileo <tavishberlin@outlook.com>
2017-11-09 08:45:29 -0600

    consolidate sigma mods

---
+++ GameData/GPP/GPP_Replacements/ExtraKerbals/Source/ExtraKerbals.cs
@@ -0,0 +1,59 @@
+﻿using System.Linq;
+using UnityEngine;
+
+
+namespace ExtraKerbals
+{
+    [KSPAddon(KSPAddon.Startup.MainMenu, false)]
+    public class ExtraKerbals : MonoBehaviour
+    {
+        public static GameObject sigma = null;
+        public static GameObject raging = null;
+
+        void Start()
+        {
+            // The 4 original kerbals
+            Transform kerbals = Resources.FindObjectsOfTypeAll<Transform>().FirstOrDefault(t => t?.name == "Kerbals" && t?.parent?.name == "OrbitScene");
+
+            // The kerbals we want to clone
+            Transform bill = kerbals.GetChild(0);
+            Transform bob = kerbals.GetChild(1);
+            Transform jeb = kerbals.GetChild(2);
+            Transform val = kerbals.GetChild(3);
+
+            // Adjust size
+            val.localScale *= 1.1f;
+
+            // Sigma Gaelan
+            sigma = Instantiate(val?.gameObject);
+            sigma.transform.SetParent(val.transform.parent);
+            sigma.transform.localPosition = (Quaternion.AngleAxis(2.5f, Vector3.up) * val.transform.localPosition) * 1.1f;
+            sigma.transform.localScale = val.transform.localScale;
+            sigma.transform.Rotate(Vector3.forward, 180);
+            sigma.transform.position += new Vector3(2, 0, 0.75f);
+
+            // Raging Gaelan
+            raging = Instantiate(bob?.gameObject);
+            raging.transform.SetParent(val.transform.parent);
+            raging.transform.localPosition = (Quaternion.AngleAxis(-10f, Vector3.up) * val.transform.localPosition) * 0.73f;
+            raging.transform.localScale = val.transform.localScale;
+            raging.transform.Rotate(Vector3.up, 280);
+            raging.transform.position += new Vector3(-0.5f, 0, 1.5f);
+
+            // Bobbers
+            Clone(sigma.GetComponent<Bobber>(), jeb.GetComponent<Bobber>());
+            Clone(raging.GetComponent<Bobber>(), bill.GetComponent<Bobber>());
+        }
+
+        void Clone(Bobber clone, Bobber original)
+        {
+            clone.ofs1 = original.ofs1;
+            clone.ofs2 = original.ofs2;
+            clone.ofs3 = original.ofs3;
+            clone.seed = original.seed;
+            clone.val1 = original.val1;
+            clone.val2 = original.val2;
+            clone.val3 = original.val3;
+        }
+    }
+}
---
+++ GameData/GPP/GPP_Replacements/ExtraKerbals/Source/Properties/AssemblyInfoHeads.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("ExtraKerbalsHeads")]
+[assembly: AssemblyDescription("ExtraKerbals Heads for GPP")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Sigma88")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("Copyright (C) Sigma88 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: KSPAssembly("ExtraKerbalsHeads", 1, 0)]
+[assembly: KSPAssemblyDependency("SigmaReplacementsHeads", 1, 0)]
